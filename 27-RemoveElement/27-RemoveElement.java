b74f0dbbc3ace993dd222705c74f419066fb0b3e
Krishanu Goswami <54260914+KrishanuG@users.noreply.github.com>
2025-08-05 12:36:44 +0530

    Create 27-RemoveElement

---
+++ 27-RemoveElement/27-RemoveElement.java
@@ -0,0 +1,14 @@
+// Last updated: 8/5/2025, 12:36:44 PM
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int fast = 0; //pointer to iterate through every element
+        int slow = 0; // track the position where the next valid element should go
+        for (fast = 0; fast < nums.length; fast++) {
+            if (nums[fast] != val) {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+        }
+        return slow;
+    }
+}
