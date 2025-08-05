79e7fcfce116730022c1388cd762f7e32f43335f
Krishanu Goswami <54260914+KrishanuG@users.noreply.github.com>
2025-08-05 12:47:44 +0530

    Delete 521-LongestUncommonSubsequenceI directory

--- 521-LongestUncommonSubsequenceI/521-LongestUncommonSubsequenceI.java
+++
@@ -1,6 +0,0 @@
-// Last updated: 8/5/2025, 12:36:24 PM
-class Solution {
-    public int findLUSlength(String a, String b) {
-         return a.equals(b) ? -1 : Math.max(a.length(), b.length());
-    }
-}
