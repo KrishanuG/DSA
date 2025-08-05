521e9fc06244e0c6da7cffcec6b3202894390f92
Krishanu Goswami <54260914+KrishanuG@users.noreply.github.com>
2025-08-05 12:43:55 +0530

    Delete 181-EmployeesEarningMoreThanTheirManagers directory

--- 181-EmployeesEarningMoreThanTheirManagers/181-EmployeesEarningMoreThanTheirManagers.sql
+++
@@ -1,6 +0,0 @@
--- Last updated: 8/5/2025, 12:36:29 PM
-# Write your MySQL query statement below
-SELECT e.Name as Employee 
-FROM Employee as e,Employee as e1
-WHERE e.ManagerId=e1.id
-AND e.Salary>e1.Salary;
