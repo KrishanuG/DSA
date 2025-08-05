c8db4cf34a348a418928ef2060b719dcc9628d60
Krishanu Goswami <54260914+KrishanuG@users.noreply.github.com>
2025-08-05 12:40:26 +0530

    Delete 125-ValidPalindrome directory

--- 125-ValidPalindrome/125-ValidPalindrome.java
+++
@@ -1,8 +0,0 @@
-// Last updated: 8/5/2025, 12:36:36 PM
-class Solution {
-    public boolean isPalindrome(String s) {
-		s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-    
-		return (s.equals( new StringBuffer(s).reverse().toString()));
-    }
-}
