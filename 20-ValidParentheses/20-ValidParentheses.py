54416b7bc90e75d8958be505e9c12d736eabf5bb
Krishanu Goswami <54260914+KrishanuG@users.noreply.github.com>
2025-08-05 12:46:29 +0530

    Delete 20-ValidParentheses directory

--- 20-ValidParentheses/20-ValidParentheses.py
+++
@@ -1,8 +0,0 @@
-# Last updated: 8/5/2025, 12:36:56 PM
-class Solution:
-    def isValid(self, s: str) -> bool:
-        while "()" in s or "{}" in s or '[]' in s:
-            s = s.replace("()", "").replace("{}", "").replace("[]", "")
-        return s == ""
-        
-        
