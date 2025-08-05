f7f58778a2d19d869c8c9232a10a09a05c96d5dc
Krishanu Goswami <54260914+KrishanuG@users.noreply.github.com>
2025-08-05 12:33:17 +0530

    File updated at 8/5/2025, 12:33:16 PM

--- 26-RemoveDuplicatesfromSortedArray/26-RemoveDuplicatesfromSortedArray.java
+++ 26-RemoveDuplicatesfromSortedArray/26-RemoveDuplicatesfromSortedArray.java
@@ -1,13 +1,14 @@
-// Last updated: 8/5/2025, 11:38:17 AM
+// Last updated: 8/5/2025, 12:33:16 PM
 class Solution {
-    public int removeDuplicates(int[] nums) {
-        int start = 0;
-        for (int end = 1; end < nums.length; end++) {
-            if(nums[start]!= nums[end]){
-                start+=1;
-                nums[start] = nums[end];
+    public int removeElement(int[] nums, int val) {
+        int fast = 0; //pointer to iterate through every element
+        int slow = 0; // track the position where the next valid element should go
+        for (fast = 0; fast < nums.length; fast++) {
+            if (nums[fast] != val) {
+                nums[slow] = nums[fast];
+                slow++;
             }
         }
-        return start+1;
+        return slow;
     }
 }
