01e9e781771875e4465ff0e64f8d692aee96abd4
Krishanu Goswami <54260914+KrishanuG@users.noreply.github.com>
2025-08-05 12:43:21 +0530

    Delete 176-SecondHighestSalary directory

--- 176-SecondHighestSalary/176-SecondHighestSalary.sql
+++
@@ -1,5 +0,0 @@
--- Last updated: 8/5/2025, 12:36:32 PM
-# Write your MySQL query statement below
-SELECT MAX(Salary) "SecondHighestSalary"
-FROM Employee 
-WHERE Salary<(SELECT MAX(Salary) FROM Employee)
