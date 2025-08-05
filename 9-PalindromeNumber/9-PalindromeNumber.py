df97fb52075168566516d35dd0d8b1d683266472
Krishanu Goswami <54260914+KrishanuG@users.noreply.github.com>
2025-08-05 12:44:30 +0530

    Delete 9-PalindromeNumber directory

--- 9-PalindromeNumber/9-PalindromeNumber.py
+++
@@ -1,8 +0,0 @@
-# Last updated: 8/5/2025, 12:37:06 PM
-class Solution:
-    def isPalindrome(self, x: int) -> bool:
-        temp,rev= x,0
-        while temp>0:
-            rev=(rev*10)+(temp%10)
-            temp//=10
-        return rev == x
