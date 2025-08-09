a6af186031c0c6d0dd14e156dde515d9f59c675e
Krishanu Goswami <54260914+KrishanuG@users.noreply.github.com>
2025-08-09 12:13:53 +0530

    Create 1455-CheckIfaWordOccursAsaPrefixofAnyWordinaSentence

---
+++ 1455-CheckIfaWordOccursAsaPrefixofAnyWordinaSentence/1455-CheckIfaWordOccursAsaPrefixofAnyWordinaSentence.java
@@ -0,0 +1,12 @@
+// Last updated: 8/9/2025, 12:13:53 PM
+class Solution {
+    public int isPrefixOfWord(String sentence, String searchWord) {
+        String[] st = sentence.split(" ");
+        for (int i = 0; i < st.length; i++) {
+            if (st[i].startsWith(searchWord)) {
+                return (i + 1);
+            }
+        }
+        return -1;
+    }
+}
