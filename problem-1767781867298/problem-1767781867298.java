61ad89f3b49be747ed945bd6113789df18dc63da
Krishanu Goswami <54260914+KrishanuG@users.noreply.github.com>
2026-01-07 16:01:07 +0530

    Create

---
+++ problem-1767781867298/problem-1767781867298.java
@@ -0,0 +1,13 @@
+// Last updated: 1/7/2026, 4:01:07 PM
+1class Solution {
+2    public String reverseWords(String s) {
+3        String[] st = s.trim().split("\\s+");
+4        StringBuilder result = new StringBuilder();
+5        for (int i = st.length - 1; i >= 0; i--) {
+6            result.append(st[i]);
+7            if (i != 0)
+8                result.append(" ");
+9        }
+10        return result.toString();
+11    }
+12}
