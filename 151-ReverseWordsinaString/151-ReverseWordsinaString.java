51616fef6089ddfd6df9156adee087ab5e48556a
Krishanu Goswami <54260914+KrishanuG@users.noreply.github.com>
2026-04-07 13:11:17 +0530

    File updated at 4/7/2026, 1:11:17 PM

--- 151-ReverseWordsinaString/151-ReverseWordsinaString.java
+++ 151-ReverseWordsinaString/151-ReverseWordsinaString.java
@@ -1,14 +1,15 @@
-// Last updated: 4/7/2026, 1:09:27 PM
-1class Solution {
-2    public String reverseWords(String s) {
-3        String[] st = s.trim().split("\\s+");
-4        StringBuilder sb = new StringBuilder();
-5        for (int i = st.length - 1; i >= 0; i--) {
-6            sb.append(st[i]);
-7            if (i > 0) {
-8                sb.append(" ");
-9            }
-10        }
-11        return sb.toString();
-12    }
-13}
+// Last updated: 4/7/2026, 1:11:17 PM
+class Solution {
+    public String reverseWords(String s) {
+        String[] words = s.trim().split("\\s+");
+        StringBuilder result = new StringBuilder();
+        for(int i= words.length -1; i>= 0; i--){
+            result.append(words[i]);
+            if(i !=0){
+                result.append(" ");
+
+            }
+        }
+        return result.toString();
+    }
+}
