eedd27669b2de1211607bc2fc52665bc91d67c33
Krishanu Goswami <54260914+KrishanuG@users.noreply.github.com>
2026-04-08 16:18:47 +0530

    File updated at 4/8/2026, 4:18:47 PM

--- 53-MaximumSubarray/53-MaximumSubarray.java
+++ 53-MaximumSubarray/53-MaximumSubarray.java
@@ -1,28 +1,16 @@
-// Last updated: 4/8/2026, 2:06:33 PM
+// Last updated: 4/8/2026, 4:18:47 PM
 1class Solution {
-2    public int maxSubArray(int[] nums) {
-3        int maxSum = nums[0];
-4        int currSum = nums[0];
-5        for (int i = 1; i < nums.length; i++) {
-6            currSum = Math.max(nums[i], currSum + nums[i]);
-7            maxSum = Math.max(maxSum, currSum);
-8        }
-9        return maxSum;
-10    }
-11}
-12
-13//another approach
-14// class Solution {
-15//     public int maxSubArray(int[] nums) {
-16//         int sum = 0;
-17//         int maxSum = Integer.MIN_VALUE;
-18
-19//         for (int num : nums) {
-20//             sum += num;
-21//             maxSum = sum > maxSum ? sum : maxSum;
-22//             sum = sum < 0 ? 0 : sum;
-23//         }
-24
-25//         return maxSum;
-26//     }
-27// }
+2    public int maxProfit(int[] prices) {
+3        //input [7,1,5,3,6,4] , output 5
+4        int buy = prices[0];
+5        int profit = 0;
+6        for (int i = 1; i < prices.length; i++) {
+7            if (prices[i] < buy) {
+8                buy = prices[i];
+9            } else if (prices[i] - buy > profit) {
+10                profit = prices[i] - buy;
+11            }
+12        }
+13        return profit;
+14    }
+15}
