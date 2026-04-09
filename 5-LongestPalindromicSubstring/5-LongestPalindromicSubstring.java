441e6ec03e93bfe8e8946fe63a8bfdb3925cf318
Krishanu Goswami <54260914+KrishanuG@users.noreply.github.com>
2026-04-09 14:23:43 +0530

    File updated at 4/9/2026, 2:23:43 PM

--- 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.java
+++ 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.java
@@ -1,35 +1,32 @@
-// Last updated: 4/9/2026, 2:16:25 PM
+// Last updated: 4/9/2026, 2:23:43 PM
 1class Solution {
 2    public String longestPalindrome(String s) {
-3        if(s.length()<=1){
-4            return s;
-5        }
-6
-7        int maxLen=1;
-8        String maxStr=s.substring(0,1);
-9
-10        for(int i=0; i<s.length(); i++){
-11            for(int j=i+1; j<=s.length();j++){
-12                if(j-i>maxLen && isPalindrome(s.substring(i,j))){
-13                    maxLen= j-i;
-14                    maxStr= s.substring(i,j);
-15                }
+3        if (s == null || s.length() < 1) return "";
+4        
+5        int start = 0, end = 0;
+6        
+7        for (int i = 0; i < s.length(); i++) {
+8            int len1 = expand(s, i, i);     // odd length
+9            int len2 = expand(s, i, i + 1); // even length
+10            
+11            int len = Math.max(len1, len2);
+12            
+13            if (len > end - start) {
+14                start = i - (len - 1) / 2;
+15                end = i + len / 2;
 16            }
 17        }
-18        return maxStr;
-19    }
-20
-21    private boolean isPalindrome(String str){
-22        int left=0;
-23        int right= str.length()-1;
-24
-25        while(left<right){
-26            if(str.charAt(left)!=str.charAt(right)){
-27                return false;
-28            }
-29            left++;
-30            right--;
-31        }
-32        return true;
-33    }
-34}
+18        
+19        return s.substring(start, end + 1);
+20    }
+21    
+22    private int expand(String s, int left, int right) {
+23        while (left >= 0 && right < s.length() 
+24               && s.charAt(left) == s.charAt(right)) {
+25            left--;
+26            right++;
+27        }
+28        
+29        return right - left - 1; // length of palindrome
+30    }
+31}
