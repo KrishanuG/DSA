4d35b9bb201f7de57149735de34ad1790aaa167a
Krishanu Goswami <54260914+KrishanuG@users.noreply.github.com>
2026-04-09 13:58:21 +0530

    File updated at 4/9/2026, 1:58:21 PM

--- 82-RemoveDuplicatesfromSortedListII/82-RemoveDuplicatesfromSortedListII.java
+++ 82-RemoveDuplicatesfromSortedListII/82-RemoveDuplicatesfromSortedListII.java
@@ -1,4 +1,4 @@
-// Last updated: 4/9/2026, 1:44:02 PM
+// Last updated: 4/9/2026, 1:58:21 PM
 1/**
 2 * Definition for singly-linked list.
 3 * class ListNode {
@@ -12,17 +12,43 @@
 11 */
 12public class Solution {
 13    public ListNode detectCycle(ListNode head) {
-14        HashSet<ListNode> visited = new HashSet<>();
-15        ListNode current = head;
-16        Integer pos = 0;
-17        while (current != null) {
-18            if (visited.contains(current)) {
-19                return current;
-20            }
-21            visited.add(current);
-22            current = current.next;
-23        }
-24        return null;
-25    }
-26
-27}
+14        if (head == null) return null;
+15        ListNode slow = head;
+16        ListNode fast = head;
+17
+18        // Step 1: Detect cycle
+19        while (fast != null && fast.next != null) {
+20            slow = slow.next;
+21            fast = fast.next.next;
+22
+23            if (slow == fast) {
+24                // Step 2: Find cycle start
+25                ListNode ptr = head;
+26                while (ptr != slow) {
+27                    ptr = ptr.next;
+28                    slow = slow.next;
+29                }
+30                return ptr;
+31            }
+32        }
+33        return null;
+34    }
+35}
+36
+37/*
+38public class Solution {
+39    public ListNode detectCycle(ListNode head) {
+40        HashSet<ListNode> visited = new HashSet<>();
+41        ListNode current = head;
+42        Integer pos = 0;
+43        while (current != null) {
+44            if (visited.contains(current)) {
+45                return current;
+46            }
+47            visited.add(current);
+48            current = current.next;
+49        }
+50        return null;
+51    }
+52}*/
+53
