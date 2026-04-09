80fa1f5bf7b545191c00ac33872e70d33524802d
Krishanu Goswami <54260914+KrishanuG@users.noreply.github.com>
2026-04-09 13:43:51 +0530

    Create 82-RemoveDuplicatesfromSortedListII

---
+++ 82-RemoveDuplicatesfromSortedListII/82-RemoveDuplicatesfromSortedListII.java
@@ -0,0 +1,28 @@
+// Last updated: 4/9/2026, 1:43:50 PM
+1/**
+2 * Definition for singly-linked list.
+3 * class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode(int x) {
+7 *         val = x;
+8 *         next = null;
+9 *     }
+10 * }
+11 */
+12public class Solution {
+13    public ListNode detectCycle(ListNode head) {
+14        HashSet<ListNode> visited = new HashSet<>();
+15        ListNode current = head;
+16        Integer pos = 0;
+17        while (current != null) {
+18            if (visited.contains(current)) {
+19                return current;
+20            }
+21            visited.add(current);
+22            current = current.next;
+23        }
+24        return null;
+25    }
+26
+27}
