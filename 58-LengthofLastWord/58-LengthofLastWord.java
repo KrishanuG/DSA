d5f630078cd1bf60becafdbc8b00b5740991b83e
Krishanu Goswami <54260914+KrishanuG@users.noreply.github.com>
2026-04-10 11:55:37 +0530

    File updated at 4/10/2026, 11:55:37 AM

--- 58-LengthofLastWord/58-LengthofLastWord.java
+++ 58-LengthofLastWord/58-LengthofLastWord.java
@@ -1,7 +1,14 @@
-// Last updated: 4/10/2026, 11:52:50 AM
-1class Solution {
-2    public int lengthOfLastWord(String s) {
-3        String[] str = s.trim().split("\\s+");
-4        return str[str.length - 1].length(); 
-5    }
-6}
+// Last updated: 4/10/2026, 11:55:37 AM
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int start=0;
+        for(int i=s.length()-1;i>=0;i--){
+            if(start==0 && s.charAt(i)==' ')
+                continue;
+            if(s.charAt(i)==' ')
+                break;
+            start++;
+        }
+        return start;
+    }
+}
