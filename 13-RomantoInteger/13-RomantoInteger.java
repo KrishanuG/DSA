f31f8eb0c3c3d828ad8daee87f797c95ae75b290
Krishanu Goswami <54260914+KrishanuG@users.noreply.github.com>
2026-04-13 18:22:22 +0530

    File updated at 4/13/2026, 6:22:22 PM

--- 13-RomantoInteger/13-RomantoInteger.java
+++ 13-RomantoInteger/13-RomantoInteger.java
@@ -1,26 +1,32 @@
-// Last updated: 4/10/2026, 11:39:54 AM
+// Last updated: 4/13/2026, 6:22:22 PM
 1class Solution {
-2    public int romanToInt(String s) {
-3        int[] a = new int[26];
-4        a['I' - 'A'] = 1;
-5        a['V' - 'A'] = 5;
-6        a['X' - 'A'] = 10;
-7        a['L' - 'A'] = 50;
-8        a['C' - 'A'] = 100;
-9        a['D' - 'A'] = 500;
-10        a['M' - 'A'] = 1000;
-11
-12        int prev = 0;
-13        int result = 0;
-14        for (int i = s.length() - 1; i >= 0; i--) {
-15            int curr = a[s.charAt(i) - 'A'];
-16            if (curr < prev) {
-17                result = result - curr;
-18            } else {
-19                result = result + curr;
-20            }
-21            prev = curr;
-22        }
-23        return result;
-24    }
-25}
+2    public int myAtoi(String s) {
+3        String str = s.trim();
+4        int i = 0, sign = 1;
+5        long ans = 0;
+6        if (str.length() == 0)
+7            return 0;
+8
+9        if (str.charAt(i) == '+' || str.charAt(i) == '-') {
+10            sign = (str.charAt(i) == '-') ? -1 : 1;
+11            i++;
+12        }
+13
+14        while (i < str.length()) {
+15            char ch = str.charAt(i);
+16            if (ch < '0' || ch > '9')
+17                break;
+18            ans = ans * 10 + (ch - '0');
+19            // Overflow check
+20            if (sign == 1 && ans > Integer.MAX_VALUE) {
+21                return Integer.MAX_VALUE;
+22            }
+23            if (sign == -1 && ans > (long)Integer.MAX_VALUE + 1) {
+24                return Integer.MIN_VALUE;
+25            }
+26            i++;
+27        }
+28
+29        return (int) (ans * sign);
+30    }
+31}
