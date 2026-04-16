455d8cdc4e57b6ec8ff4ecb73f047481235d702f
Krishanu Goswami <54260914+KrishanuG@users.noreply.github.com>
2026-04-16 14:00:03 +0530

    File updated at 4/16/2026, 2:00:03 PM

--- 344-ReverseString/344-ReverseString.java
+++ 344-ReverseString/344-ReverseString.java
@@ -1,56 +1,22 @@
-// Last updated: 4/16/2026, 1:59:45 PM
-class Solution {
-    static {
-        for(int i = 0; i < 500; ++i) {
-            totalFruit(new int[] {1, 2, 1, 2, 2, 2, 2, 3, 4, 4, 5, 6, 7, 8, 5, 4});
-        }
-    }
-    public static int totalFruit(int[] fruits) {
-        if(fruits.length == 1) return 1;
-
-        int i = 0;
-        int j = 0;
-        int f1 = fruits[0];
-        int f1i = 0;
-        int f2 = -1;
-        int f2i = -1;
-
-        while(j < fruits.length) {
-            if(fruits[j] == f1) {
-                f1i = j;
-            }else if(fruits[j] == f2) {
-                f2i = j;
-            }else if(f2 == -1) {
-                f2 = fruits[j];
-                f2i = j;
-            }else {
-                break;
-            }
-            j += 1;
-        }
-        int maxFruits = j - i;
-
-        while(j < fruits.length) {
-            if(fruits[j] == f1) {
-                f1i = j;
-            }else if(fruits[j] == f2) {
-                f2i = j;
-            }else {
-                if(f1i < f2i) {
-                    f1 = fruits[j];
-                    i = f1i + 1;
-                    f1i = j;
-                }else {
-                    f2 = fruits[j];
-                    i = f2i + 1;
-                    f2i = j;
-                }
-            }
-
-            maxFruits = Math.max(maxFruits, j - i + 1);
-            j += 1;
-        }
-
-        return maxFruits;
-    }
-}
+// Last updated: 4/16/2026, 2:00:03 PM
+1class Solution {
+2    public int totalFruit(int[] fruits) {
+3        int start = 0, end = 0;
+4        int n = fruits.length, maxLen = 0;
+5        Map<Integer, Integer> map = new HashMap<>();
+6        while (end < n) {
+7            map.put(fruits[end], map.getOrDefault(fruits[end], 0) + 1);
+8            while (map.size() > 2) {
+9                map.put(fruits[start], map.get(fruits[start]) - 1);
+10                if (map.get(fruits[start]) == 0) {
+11                    map.remove(fruits[start]);
+12                }
+13                start++;
+14            }
+15            int currLen = end - start + 1;
+16            maxLen = Math.max(maxLen, currLen);
+17            end++;
+18        }
+19        return maxLen;
+20    }
+21}
