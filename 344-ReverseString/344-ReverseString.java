ef95d21e22c725d323216341075f4c8c8e904f02
Krishanu Goswami <54260914+KrishanuG@users.noreply.github.com>
2026-04-16 13:59:18 +0530

    File updated at 4/16/2026, 1:59:18 PM

--- 344-ReverseString/344-ReverseString.java
+++ 344-ReverseString/344-ReverseString.java
@@ -1,15 +1,22 @@
-// Last updated: 4/16/2026, 12:19:50 PM
+// Last updated: 4/16/2026, 1:59:18 PM
 1class Solution {
-2    public void reverseString(char[] s) {
-3        int start = 0;
-4        int end = s.length - 1;
-5
-6        while (start < end) {
-7            char temp = s[start];
-8            s[start] = s[end];
-9            s[end] = temp;
-10            start++;
-11            end--;
-12        }
-13    }
-14}
+2    public int totalFruit(int[] fruits) {
+3        int start = 0, end = 0;
+4        int n = fruits.length, maxLen = 0;
+5        Map<Integer, Integer> map = new HashMap<>();
+6        while (end < n) {
+7            map.put(fruits[end], map.getOrDefault(fruits[end], 0) + 1);
+8            while (map.size() > 2) {
+9                map.put(fruits[start], map.get(fruits[start]) - 1);
+10                if (map.get(fruits[start]) == 0) {
+11                    map.remove(fruits[start]);
+12                }
+13                start++;
+14            }
+15            int currLen = end - start + 1;
+16            maxLen = Math.max(maxLen, currLen);
+17            end++;
+18        }
+19        return maxLen;
+20    }
+21}
