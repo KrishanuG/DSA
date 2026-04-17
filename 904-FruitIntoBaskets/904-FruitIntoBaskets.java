de85b5bc1123f9aa2f7b501b1bda3f59fd0748a4
Krishanu Goswami <54260914+KrishanuG@users.noreply.github.com>
2026-04-17 14:11:11 +0530

    File updated at 4/17/2026, 2:11:11 PM

--- 904-FruitIntoBaskets/904-FruitIntoBaskets.java
+++ 904-FruitIntoBaskets/904-FruitIntoBaskets.java
@@ -1,22 +1,13 @@
-// Last updated: 4/17/2026, 2:03:04 PM
+// Last updated: 4/17/2026, 2:11:11 PM
 1class Solution {
-2    public int totalFruit(int[] fruits) {
-3        int start = 0, end = 0;
-4        int n = fruits.length, maxLen = 0;
-5        HashMap<Integer, Integer> freq = new HashMap<>();
-6        while (end < n) {
-7            freq.put(fruits[end], freq.getOrDefault(fruits[end], 0) + 1);
-8            while (freq.size() > 2) {
-9                freq.put(fruits[start], freq.get(fruits[start]) - 1);
-10                if (freq.get(fruits[start]) == 0) {
-11                    freq.remove(fruits[start]);
-12                }
-13                start++;
-14            }
-15            int currLen = end - start + 1;
-16            maxLen = Math.max(maxLen, currLen);
-17            end++;
-18        }
-19        return maxLen;
-20    }
-21}
+2    public String reverseWords(String s) {
+3        String[] words = s.split("\\s+");
+4        StringBuilder sb = new StringBuilder();
+5        for(String word : words){
+6            StringBuilder revWord = new StringBuilder(word).reverse();
+7            sb.append(revWord).append(" ");
+8        }
+9    
+10        return sb.toString().trim();
+11    }
+12}
