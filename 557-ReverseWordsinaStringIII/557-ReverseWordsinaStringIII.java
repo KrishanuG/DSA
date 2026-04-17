240d624f0ac2d95f8b352eb5675049a20c6152ac
Krishanu Goswami <54260914+KrishanuG@users.noreply.github.com>
2026-04-17 14:12:09 +0530

    Create 557-ReverseWordsinaStringIII

---
+++ 557-ReverseWordsinaStringIII/557-ReverseWordsinaStringIII.java
@@ -0,0 +1,12 @@
+// Last updated: 4/17/2026, 2:12:09 PM
+1class Solution {
+2    public String reverseWords(String s) {
+3        String[] words = s.split(" ");
+4        StringBuilder sb = new StringBuilder();
+5        for(String word : words){
+6            StringBuilder revWord = new StringBuilder(word).reverse();
+7            sb.append(revWord).append(" ");
+8        }
+9        return sb.toString().trim();
+10    }
+11}
